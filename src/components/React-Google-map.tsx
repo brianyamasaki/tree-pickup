39a6481fc1c43656095b27652607b1faad053bbd
Brian Yamasaki <brian.yamasaki@gmail.com>
2024-02-01 15:59:46 -0800

    Polygons showing

--- src/components/React-Google-map.tsx
+++ src/components/React-Google-map.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+import { APIProvider, Map, Marker, useMap, useMapsLibrary  } from "@vis.gl/react-google-maps";
+import { Polygon } from './MapsPolygon.tsx';
+import pickupRegion from './pickup-region.json';
 
 const API_KEY = import.meta.env.PUBLIC_MAPS_KEY;
 export type Position = {
   lat: number;
   lng: number;
 };
+
 export type Place = {
   id: number;
   name: string;
@@ -25,6 +28,7 @@ const posMcAuliffe: Position = {
 };
 
 const ReactGoogleMap = ({places}: Props) => {
+  
   return (
     <div className="map" style={{ width: "500px", height: "500px" }}>
       <APIProvider apiKey={API_KEY}>
@@ -34,6 +38,11 @@ const ReactGoogleMap = ({places}: Props) => {
           gestureHandling={"greedy"}
           disableDefaultUI={true}
         >
+          <Polygon 
+            strokeColor="#ff0000"
+            strokeWeight={1.5}
+            paths={pickupRegion}
+          />
           {places.map(place => (
               <Marker 
                 key={place.id}
