788350a9375a42f0c1df7eed1349a8da3ecf5328
Brian Yamasaki <brian.yamasaki@gmail.com>
2024-02-01 18:58:10 -0800

    Polygon shapes show up on Google Map

--- src/components/React-Google-map.tsx
+++ src/components/React-Google-map.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { APIProvider, Map, Marker, useMap, useMapsLibrary  } from "@vis.gl/react-google-maps";
 import { Polygon } from './MapsPolygon.tsx';
-import pickupRegion from './pickup-region.json';
+import pickupRegion from '../data/pickup-region.json';
+import pinkRegion from '../data/pink-region.json';
+import redRegion from '../data/red-region.json';
+import purpleRegion from '../data/purple-region.json';
+import greenRegion from '../data/green-region.json';
+import tealRegion from '../data/teal-region.json';
+import blueRegion from '../data/blue-region.json';
+import navyRegion from '../data/navy-region.json';
+import brownRegion from '../data/brown-region.json';
+import orangeRegion from '../data/orange-region.json';
 
 const API_KEY = import.meta.env.PUBLIC_MAPS_KEY;
+
 export type Position = {
   lat: number;
   lng: number;
+};
+
+type OurRegion = {
+  path: Position[];
+  fill: string;
 };
 
 export type Place = {
@@ -21,11 +36,51 @@ type Props = {
   places: Place[];
 };
 
+const regions: OurRegion[] = [
+  {
+    path: pinkRegion,
+    fill: 'pink'
+  },
+  {
+    path: redRegion,
+    fill: 'red'
+  },
+  {
+    path: greenRegion,
+    fill: 'green'
+  },
+  {
+    path: purpleRegion,
+    fill: 'purple'
+  },
+  {
+    path: tealRegion,
+    fill: '#00ffff'
+  },
+  {
+    path: blueRegion,
+    fill: 'blue'
+  },
+  {
+    path: navyRegion,
+    fill: 'navy'
+  },
+  {
+    path: brownRegion,
+    fill: 'brown'
+  },
+  {
+    path: orangeRegion,
+    fill: 'orange'
+  }
+]
+
 // Latitude / Longitude of McAuliffe Park, Kirkland, WA
 const posMcAuliffe: Position = {
   lat: 47.703946,
   lng: -122.197332,
 };
+
 
 const ReactGoogleMap = ({places}: Props) => {
   
@@ -38,11 +93,15 @@ const ReactGoogleMap = ({places}: Props) => {
           gestureHandling={"greedy"}
           disableDefaultUI={true}
         >
-          <Polygon 
-            strokeColor="#ff0000"
-            strokeWeight={1.5}
-            paths={pickupRegion}
-          />
+          {regions.map(region => (
+            <Polygon
+              fillColor={region.fill}
+              paths={region.path}
+              strokeWeight={1}
+              strokeOpacity={0.5}
+            />
+          ))
+          }
           {places.map(place => (
               <Marker 
                 key={place.id}
